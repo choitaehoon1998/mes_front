bd285291ba73a86c916d920ab024dcd856b9f077
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-02-16 10:13:58 +0900

    App js router 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import ContractManagement from "./component/ContractManagement";
 import AdstrativeManagement from "./component/AdstrativeManagement";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <ContractManagement />
-    </>
+    <Routes>
+      <Route path="/" element={<ContractManagement />} />
+      <Route path="/adstr" element={<AdstrativeManagement />} />
+    </Routes>
   );
 }
 
--- src/form/formHeader.js
+++ src/form/formHeader.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const formHeader = () => {
+const formHeader = (props) => {
   return (
     <div className="app-page-header">
-      <h3 className="mb-0 mr-3 font-weight-bold">계약관리</h3>
+      <h3 className="mb-0 mr-3 font-weight-bold">{props.children}</h3>
     </div>
   );
 };
